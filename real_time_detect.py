fb2047b6b5a38c866422006db772885617f69d80
Muggalla Rahul <rahulmuggalla02@protonmail.com>
2023-06-07 18:16:23 +0530

    Update real_time_detect.py

--- real_time_detect.py
+++ real_time_detect.py
@@ -18,13 +18,6 @@ cv2.setMouseCallback('RGB', RGB)
 cap=cv2.VideoCapture(0)
 
 
-my_file = open("coco.txt", "r")
-data = my_file.read()
-class_list = data.split("\n")
-print(class_list)
-count=0
-
-
 while True:    
     ret,frame = cap.read()   
     count += 1
@@ -42,17 +35,9 @@ while True:
     for index,row in px.iterrows():
 #        print(row)
 
-        x1=int(row[0])
-        y1=int(row[1])
-        x2=int(row[2])
-        y2=int(row[3])
         d=int(row[5])
         c=class_list[d]
         print(c)
         cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2) 
         cv2.putText(frame,str(c),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,0,0),1)
     cv2.imshow("RGB", frame)
-    if cv2.waitKey(1)&0xFF==27:
-        break
-cap.release()
-cv2.destroyAllWindows()
